ddb185e8b28417baddaf397cb40cdd4efcba272b
Cormac <cormo905@gmail.com>
2025-09-26 14:09:41 +1000

    Added XML documentation

--- UnityExtensions.cs
+++ UnityExtensions.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public static class UnityExtensions
 {
+    /// <summary>
+    /// Checks whether a LayerMask contains a given layer
+    /// </summary>
+    /// <param name="layer">The layer to test for</param>
+    /// <returns>Whether the LayerMask includes the given layer</returns>
     public static bool Contains(this LayerMask layerMask, int layer)
     {
         // Create a mask with only the layer we are checking
@@ -13,18 +18,32 @@ public static class UnityExtensions
         return isolatedMask != 0;
     }
 
+    /// <summary>
+    /// Squares the value
+    /// </summary>
+    /// <returns>The value squared</returns>
     public static float Sqr(this float f)
     {
         return f * f;
     }
 
+    /// <summary>
+    /// Checks whether the value lies between two values
+    /// </summary>
+    /// <param name="min">The minimum bounds of the range</param>
+    /// <param name="max">The maximum bounds of the range</param>
+    /// <returns>Whether the value is between min and max (inclusive)</returns>
     public static bool Between(this float f, float min, float max)
     {
         return f >= min && f <= max;
     }
 
+    /// <summary>
+    /// Checks whether the value lies between 0 and 1
+    /// </summary>
+    /// <returns>Whether the value is between 0 and 1</returns>
     public static bool Between01(this float f)
     {
-        return f >= 0f && f <= 1f;
+        return Between(0f, 1f);
     }
 }
--- VectorExtensions.cs
+++ VectorExtensions.cs
@@ -2,18 +2,38 @@ using UnityEngine;
 
 public static class VectorExtensions
 {
+    // ----- Vector 3 -----
+    /// <summary>Zeroes the x-axis of the vector</summary>
     public static Vector3 ZeroX(this Vector3 vector) => new Vector3(0f, vector.y, vector.z);
+    /// <summary>Zeroes the y-axis of the vector</summary>
     public static Vector3 ZeroY(this Vector3 vector) => new Vector3(vector.x, 0f, vector.z);
+    /// <summary>Zeroes the z-axis of the vector</summary>
     public static Vector3 ZeroZ(this Vector3 vector) => new Vector3(vector.x, vector.y, 0f);
+    /// <summary>Sets the x-axis of the vector</summary>
+    /// <param name="value">The value to set the x-axis to</param>
     public static Vector3 SetX(this Vector3 vector, float value) => new Vector3(value, vector.y, vector.z);
+    /// <summary>Sets the y-axis of the vector</summary>
+    /// <param name="value">The value to set the y-axis to</param>
     public static Vector3 SetY(this Vector3 vector, float value) => new Vector3(vector.x, value, vector.z);
+    /// <summary>Sets the z-axis of the vector</summary>
+    /// <param name="value">The value to set the z-axis to</param>
     public static Vector3 SetZ(this Vector3 vector, float value) => new Vector3(vector.x, vector.y, value);
 
+    // ----- Vector 2 -----
+    /// <summary>Zeroes the x-axis of the vector</summary>
     public static Vector2 ZeroX(this Vector2 vector) => new Vector2(0f, vector.y);
+    /// <summary>Zeroes the y-axis of the vector</summary>
     public static Vector2 ZeroY(this Vector2 vector) => new Vector2(vector.x, 0f);
+    /// <summary>Creates an equivalent Vector3 with a zeroed z</summary>
     public static Vector3 ZeroZ(this Vector2 vector) => new Vector3(vector.x, vector.y, 0f);
+    /// <summary>Sets the x-axis of the vector</summary>
+    /// <param name="value">The value to set the x-axis to</param>
     public static Vector2 SetX(this Vector2 vector, float value) => new Vector2(value, vector.y);
+    /// <summary>Sets the y-axis of the vector</summary>
+    /// <param name="value">The value to set the y-axis to</param>
     public static Vector2 SetY(this Vector2 vector, float value) => new Vector2(vector.x, value);
+    /// <summary>Creates an equivalent Vector3 with z set to value</summary>
+    /// <param name="value">The value to set the z-axis to</param>
     public static Vector3 SetZ(this Vector2 vector, float value) => new Vector3(vector.x, vector.y, value);
     
     /// <summary>
